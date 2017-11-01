394745dca4f4187c4c840b39df1a3b9921f044c0
NagarD <nagard11@gmail.com>
2017-11-01 22:47:00 +0200

    Ініціальзація проекту

--- migrations/m170914_101049_create_deploy_table.php
+++ migrations/m170914_101049_create_deploy_table.php
@@ -7,27 +7,28 @@ use yii\db\Migration;
  */
 class m170914_101049_create_deploy_table extends Migration
 {
-    /**
-     * @inheritdoc
-     */
-    public function up()
-    {
-        $this->createTable('deploy', [
-            'id' => $this->primaryKey(),
-            'request_ip' => $this->string(255),
-            'request_data' => $this->text(),
-            'request_url' => $this->string(255),
+	/**
+	 * @inheritdoc
+	 */
+	public function up()
+	{
+		$this->createTable('deploy', [
+			'id' => $this->primaryKey(),
+			'request_ip' => $this->string(255),
+			'request_data' => $this->text(),
+			'request_url' => $this->string(255),
+			'message' => $this->text(),
 			'created_at' => $this->integer(),
 			'updated_at' => $this->integer(),
-            'status' => $this->integer()->defaultValue(0),
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function down()
-    {
-        $this->dropTable('deploy');
-    }
+			'status' => $this->integer()->defaultValue(0),
+		]);
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function down()
+	{
+		$this->dropTable('deploy');
+	}
 }

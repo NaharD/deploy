7c52f5d16122fbe92a360addfc94cfa4837d1688
Володимир <nagard11@gmail.com>
2018-12-02 21:29:08 +0200

    Update Module.php
    Нові адреси для bitbucked

--- Module.php
+++ Module.php
@@ -36,7 +36,7 @@ class Module extends \yii\base\Module
 	public $ipFilters = [
 		'bitbucked' => [
 			'class' => 'nahard\deploy\models\forms\DeployBitbucketForm',
-			'ranges' => ['104.192.136.0/21', '34.198.203.127', '34.198.178.64', '34.198.32.85'],
+			'ranges' => ['13.52.5.0/25', '18.234.32.128/25', '18.205.93.0/25'],
 		],
 		'manual' => [
 			'class' => 'nahard\deploy\models\forms\DeployManualForm',
